4b96aeac67c17c39646ca9bddaa57f700c6374d4
ayushigarg1405 <34615492+ayushigarg1405@users.noreply.github.com>
2020-06-06 11:28:47 +0530

    Day 05 solution

---
+++ Day_05_Random_pick_with-weights.cpp
@@ -0,0 +1,47 @@
+**Question**
+
+Given an array w of positive integers, where w[i] describes the weight of index i, write a function pickIndex which randomly picks an index in proportion to its weight.
+
+Note:
+
+1 <= w.length <= 10000
+1 <= w[i] <= 10^5
+pickIndex will be called at most 10000 times.
+Example 1:
+
+Input: 
+["Solution","pickIndex"]
+[[[1]],[]]
+Output: [null,0]
+
+**Solution Approach**
+
+Making use of Binary Search
+
+**Solution**
+
+class Solution {
+public:
+    vector<int> s;
+    Solution(vector<int>& w) {
+        for (int ind : w){
+            if(s.empty())
+                s.push_back(ind);
+            else
+                s.push_back(ind + s.back());
+        }
+    }
+    
+    int pickIndex() {
+        int x = s.back();
+        int index = rand() % x;
+        auto it = upper_bound(s.begin(), s.end(),index);
+        return it - s.begin();
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(w);
+ * int param_1 = obj->pickIndex();
+ */
